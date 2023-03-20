9ffa51535e8b114e35dbb34a183d3e57d5953560
XsTayLXneKit <azabitiy@gmail.com>
2023-03-19 22:27:53 -0400

    2nd

--- binary.py
+++ binary.py
@@ -1,9 +1,7 @@
 def parse_binary(binary):
-    # Check if the input is a valid binary number
     if not all(char in '01' for char in binary):
-        raise ValueError("Invalid binary input")
+        raise ValueError(f"Invalid binary literal: {binary}")
 
-    # Convert the binary number to decimal
     decimal = 0
     for i in range(len(binary)):
         digit = binary[-i-1]
